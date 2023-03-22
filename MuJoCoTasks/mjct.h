e5cf4f50b01da2c44b17c86157281948aecc28b7
ianmi <ian.github@pronovost.net>
2023-03-22 12:36:12 -0400

    made option to render

--- MuJoCoTasks/mjct.h
+++ MuJoCoTasks/mjct.h
@@ -21,103 +21,121 @@ public:
 	int H;
 	int W;
 	GLFWwindow* window;
+	bool b_render = false;
 
-	void create(const char* taskname) {
+	void create(const char* taskname, bool render = false) {
 
-		if (!glfwInit())
-		{
-			std::cout << "!glfwInit";
-		}
-		//make single buffer and invisible window
-		glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
-		glfwWindowHint(GLFW_VISIBLE, 0);
-		window = glfwCreateWindow(800, 800, "MuJoCoTask", NULL, NULL);
-		if (!window) {
-			mju_error("Could not create GLFW window");
-		}
+		b_render = render;
 
-		// make context current
-		glfwMakeContextCurrent(window);
+		//necessary init
 		char error[100] = "Could not load model";
 		m = mj_loadXML(taskname, 0, error, 100);
 		d = mj_makeData(m);
-		mjv_defaultCamera(&cam);
 		mjv_defaultOption(&opt);
-		mjv_defaultScene(&scn);
-		mjr_defaultContext(&con);
 
-		//user defined settings
-		mjv_makeScene(m, &scn, 1000);
-		mjr_makeContext(m, &con, 200);
+		if (render) {
 
-		viewport = mjr_maxViewport(&con);
-		W = viewport.width;
-		H = viewport.height;
+			if (!glfwInit())
+			{
+				std::cout << "!glfwInit";
+			}
+			//make single buffer and invisible window
+			glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
+			glfwWindowHint(GLFW_VISIBLE, 0);
+			window = glfwCreateWindow(800, 800, "MuJoCoTask", NULL, NULL);
+			if (!window) {
+				mju_error("Could not create GLFW window");
+			}
 
-		rgb = (unsigned char*)std::malloc(3 * W * H);
-		depth = (float*)std::malloc(sizeof(float) * W * H);
-		if (!rgb || !depth) {
-			mju_error("Could not allocate buffers");
+			// make context current
+			glfwMakeContextCurrent(window);
+
+			mjv_defaultCamera(&cam);
+			mjv_defaultScene(&scn);
+			mjr_defaultContext(&con);
+
+			//user defined settings
+			mjv_makeScene(m, &scn, 1000);
+			mjr_makeContext(m, &con, 200);
+
+			viewport = mjr_maxViewport(&con);
+			W = viewport.width;
+			H = viewport.height;
+
+			rgb = (unsigned char*)std::malloc(3 * W * H);
+			depth = (float*)std::malloc(sizeof(float) * W * H);
+			if (!rgb || !depth) {
+				mju_error("Could not allocate buffers");
+			}
 		}
-	}
-
-	// deallocate everything
-	void closeMuJoCo(void)
-	{
-		mj_deleteData(d);
-		mj_deleteModel(m);
-		mjr_freeContext(&con);
-		mjv_freeScene(&scn);
-	}
-
-	void closeOpenGL()
-	{
-		glfwTerminate();
 	}
 
 	void setup_camera(double lookat[3], mjtNum azimuth, mjtNum elevation, mjtNum distance) {
-		//camera position
-		for (int i = 0; i < 3; i++)
+		if (render) 
 		{
-			cam.lookat[i] = lookat[i];
+			//camera position
+			for (int i = 0; i < 3; i++)
+			{
+				cam.lookat[i] = lookat[i];
+			}
+			//camera angle
+			cam.azimuth = azimuth;
+			cam.elevation = elevation;
+			//camera distance
+			cam.distance = distance;
 		}
-		//camera angle
-		cam.azimuth = azimuth;
-		cam.elevation = elevation;
-		//camera distance
-		cam.distance = distance;
+		else 
+		{
+			//should be a Warning
+			std::cout << "This entity is off-render!";
+		}
 	}
 
 	unsigned char* render() {
-		// update abstract scene
-		mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+		if (render) 
+		{
+			// update abstract scene
+			mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
 
-		// render scene in offscreen buffer
-		mjr_render(viewport, &scn, &con);
+			// render scene in offscreen buffer
+			mjr_render(viewport, &scn, &con);
 
-		// read rgb and depth buffers
-		mjr_readPixels(rgb, depth, viewport, &con);
+			// read rgb and depth buffers
+			mjr_readPixels(rgb, depth, viewport, &con);
 
-		// create OpenCV Mat object from rgb buffer.
-		
-		//cv::Mat rgb_mat(H, W, CV_8UC3, rgb);
+			// create OpenCV Mat object from rgb buffer.
 
-		//cv::flip(rgb_mat, rgb_mat, 0);
-		//cv::cvtColor(rgb_mat, rgb_mat, cv::COLOR_RGB2BGR);
+			//cv::Mat rgb_mat(H, W, CV_8UC3, rgb);
 
-		// save as JPEG
-		//cv::imwrite("output.jpg", rgb_mat);
-		return rgb;
+			//cv::flip(rgb_mat, rgb_mat, 0);
+			//cv::cvtColor(rgb_mat, rgb_mat, cv::COLOR_RGB2BGR);
+
+			// save as JPEG
+			//cv::imwrite("output.jpg", rgb_mat);
+			return rgb;
+		}
+		else
+		{
+			//should be a warning
+			std::cout << "Attempting to render on off-render entity!";
+		}
 	}
 
 	~FixedEnvironment() {
-		// close file, free buffers
-		std::free(rgb);
-		std::free(depth);
 
-		// close MuJoCo and OpenGL
-		closeMuJoCo();
-		closeOpenGL();
+		// close file, free buffers and OpenGL
+		if (render) 
+		{
+			std::free(rgb);
+			std::free(depth);
+			glfwTerminate();
+			mjr_freeContext(&con);
+			mjv_freeScene(&scn);
+		}
+
+		// close MuJoCo
+		mj_deleteData(d);
+		mj_deleteModel(m);
 	}
 };
 
@@ -129,9 +147,9 @@ class Tosser {
 	std::string info = "";
 	
 	//init
-	Tosser()
+	Tosser(bool render)
 	{
-		env.create("tosser.xml"); //add render argument
+		env.create("tosser.xml", render); //add render argument
 
 		//set camera position
 		double pos[] = { 0.022, -0.678, 0.393 };
@@ -218,9 +236,6 @@ class Tosser {
 	//render function
 	unsigned char* render()
 	{
-		if (true) {
-			return env.render();
-		}
+		return env.render();
 	}
-
 };
