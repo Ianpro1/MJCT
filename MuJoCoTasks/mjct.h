a81f470beb13f690127c1976cff7fc9b4ba1a939
ianmi <ian.github@pronovost.net>
2023-03-22 20:29:04 -0400

    major changes to mjct.h

--- MuJoCoTasks/mjct.h
+++ MuJoCoTasks/mjct.h
@@ -1,19 +1,36 @@
+//The goal of this header file is to contain classes that can be implemented nicely to python bindings
+
 #pragma once
 #include <iostream>
 #include <mujoco/mujoco.h>
 #include <GLFW/glfw3.h>
 #include <opencv2/opencv.hpp>
-//The goal of this header file is to contain classes that can be implemented nicely to python bindings
-//TODO: add condition to render function in tosser class
 
-class FixedEnvironment {
+//example of what components every task environment must have by default (in the documentation it is refered to as render-disabled or LightEnvironment)
+//equivalent to render-disabled environment structure
+/*struct LightEnvironment {
 
-public:
-	mjModel* m = NULL;
-	mjData* d = NULL;
-	bool b_render = false;
+	mjModel* m;
+	mjData* d;
 
-	//these are optional variables
+	LightEnvironment(const char* taskname)
+	{
+		char error[100] = "Could not load model.xml";
+		m = mj_loadXML(taskname, 0, error, 100);
+		d = mj_makeData(m);
+	}
+
+	~LightEnvironment()
+	{
+		mj_deleteData(d);
+		mj_deleteModel(m);
+	}
+};*/
+
+
+//structure used for render-enabled environments
+struct HeavyEnvironment {
+
 	mjvOption opt;
 	mjvCamera cam;
 	mjvScene scn;
@@ -25,156 +42,142 @@ public:
 	int W;
 	GLFWwindow* window;
 
-	void create(const char* taskname, bool render = false) {
+	HeavyEnvironment(mjModel* m)
+	{
+		if (!glfwInit())
+		{
+			std::cout << "!glfwInit";
+		}
 
-		b_render = render;
+		//make single buffer and invisible window
+		glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
+		glfwWindowHint(GLFW_VISIBLE, 0);
+		window = glfwCreateWindow(800, 800, "MuJoCoTask", NULL, NULL);
+		if (!window) {
+			mju_error("Could not create GLFW window.");
+		}
 
-		//necessary init
-		char error[100] = "Could not load model";
-		m = mj_loadXML(taskname, 0, error, 100);
-		d = mj_makeData(m);
+		// make context current
+		glfwMakeContextCurrent(window);
+		mjv_defaultOption(&opt); //these are visualization configs
+		mjv_defaultCamera(&cam);
+		mjv_defaultScene(&scn);
+		mjr_defaultContext(&con);
 
-		if (render) {
+		//user defined settings
+		mjv_makeScene(m, &scn, 1000);
+		mjr_makeContext(m, &con, 200);
 
-			if (!glfwInit())
-			{
-				std::cout << "!glfwInit";
-			}
-			//make single buffer and invisible window
-			glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
-			glfwWindowHint(GLFW_VISIBLE, 0);
-			window = glfwCreateWindow(800, 800, "MuJoCoTask", NULL, NULL);
-			if (!window) {
-				mju_error("Could not create GLFW window");
-			}
+		viewport = mjr_maxViewport(&con);
+		W = viewport.width;
+		H = viewport.height;
 
-			// make context current
-			glfwMakeContextCurrent(window);
-			mjv_defaultOption(&opt); //these are visualization configs
-			mjv_defaultCamera(&cam);
-			mjv_defaultScene(&scn);
-			mjr_defaultContext(&con);
-
-			//user defined settings
-			mjv_makeScene(m, &scn, 1000);
-			mjr_makeContext(m, &con, 200);
-
-			viewport = mjr_maxViewport(&con);
-			W = viewport.width;
-			H = viewport.height;
-
-			rgb = (unsigned char*)std::malloc(3 * W * H);
-			depth = (float*)std::malloc(sizeof(float) * W * H);
-			if (!rgb || !depth) {
-				mju_error("Could not allocate buffers");
-			}
+		rgb = (unsigned char*)std::malloc(3 * W * H);
+		depth = (float*)std::malloc(sizeof(float) * W * H);
+		if (!rgb || !depth) {
+			mju_error("Could not allocate buffers.");
 		}
 	}
 
 	void setup_camera(double lookat[3], mjtNum azimuth, mjtNum elevation, mjtNum distance) {
-		if (render) 
+
+		//camera position
+		for (int i = 0; i < 3; i++)
 		{
-			//camera position
-			for (int i = 0; i < 3; i++)
-			{
-				cam.lookat[i] = lookat[i];
-			}
-			//camera angle
-			cam.azimuth = azimuth;
-			cam.elevation = elevation;
-			//camera distance
-			cam.distance = distance;
+			cam.lookat[i] = lookat[i];
 		}
-		else 
-		{
-			//should be a Warning
-			std::cout << "This entity is off-render!";
-		}
+		//camera angle
+		cam.azimuth = azimuth;
+		cam.elevation = elevation;
+		//camera distance
+		cam.distance = distance;
 	}
 
-	unsigned char* render() {
-		if (render) 
-		{
-			// update abstract scene
-			mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+	unsigned char* render(mjModel* m, mjData* d)
+	{
+		// update abstract scene
+		mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
 
-			// render scene in offscreen buffer
-			mjr_render(viewport, &scn, &con);
+		// render scene in offscreen buffer
+		mjr_render(viewport, &scn, &con);
 
-			// read rgb and depth buffers
-			mjr_readPixels(rgb, depth, viewport, &con);
+		// read rgb and depth buffers
+		mjr_readPixels(rgb, depth, viewport, &con);
 
-			// create OpenCV Mat object from rgb buffer.
+		// create OpenCV Mat object from rgb buffer.
 
-			//cv::Mat rgb_mat(H, W, CV_8UC3, rgb);
+		//cv::Mat rgb_mat(H, W, CV_8UC3, rgb);
 
-			//cv::flip(rgb_mat, rgb_mat, 0);
-			//cv::cvtColor(rgb_mat, rgb_mat, cv::COLOR_RGB2BGR);
+		//cv::flip(rgb_mat, rgb_mat, 0);
+		//cv::cvtColor(rgb_mat, rgb_mat, cv::COLOR_RGB2BGR);
 
-			// save as JPEG
-			//cv::imwrite("output.jpg", rgb_mat);
-			return rgb;
-		}
-		else
-		{
-			//should be a warning
-			std::cout << "Attempting to render on off-render entity!";
-		}
+		// save as JPEG
+		//cv::imwrite("output.jpg", rgb_mat);
+		return rgb;
 	}
 
-	~FixedEnvironment() {
+	~HeavyEnvironment() {
 
 		// close file, free buffers and OpenGL
-		if (render) 
-		{
-			std::free(rgb);
-			std::free(depth);
-			glfwTerminate();
-			mjr_freeContext(&con);
-			mjv_freeScene(&scn);
-		}
-
-		// close MuJoCo
-		mj_deleteData(d);
-		mj_deleteModel(m);
+		std::free(rgb);
+		std::free(depth);
+		glfwTerminate();
+		mjr_freeContext(&con);
+		mjv_freeScene(&scn);
 	}
 };
 
 
-//this is the class that should be binded to python
+//example of basic task environment
 class Tosser {
 
-	FixedEnvironment env;
+private:
+	mjModel* m;
+	mjData* d;
+	HeavyEnvironment* env;
 	std::string info = "";
-	
-	//init
-	Tosser(bool render)
-	{
-		env.create("tosser.xml", render); //add render argument
+	bool b_render;
 
-		//set camera position
-		double pos[] = { 0.022, -0.678, 0.393 };
-		env.setup_camera(pos, -173.2, -13.4, 1.8);
+public:
+	//init
+	Tosser(bool render=false)
+	{
+		// required init
+		char error[100] = "Could not load tosser.xml";
+		m = mj_loadXML("tosser.xml", 0, error, 100);
+		d = mj_makeData(m);
+		b_render = render;
+
+		//optional init
+		if (b_render){
+			env = new HeavyEnvironment(m);
+			//set camera position
+			double pos[] = { 0.022, -0.678, 0.393 };
+			env->setup_camera(pos, -173.2, -13.4, 1.8);
+		}
+		else {
+			env = nullptr;
+		}
 	}
 
 	//step function
 	std::tuple <std::array<double, 10>, double, bool, bool, std::string> step(double action[2])
 	{
 		//process action
-		env.d->ctrl[0] = action[0];
-		env.d->ctrl[1] = action[1];
+		d->ctrl[0] = action[0];
+		d->ctrl[1] = action[1];
 
 		//step
-		mj_step(env.m, env.d);
+		mj_step(m, d);
 
 
 		//filter reward
 		double reward;
-		if (env.d->sensordata[2] > 9) {
+		if (d->sensordata[2] > 9) {
 			//red bucket
 			reward = 0.5;
 		}
-		else if (env.d->sensordata[3] > 9) {
+		else if (d->sensordata[3] > 9) {
 			//green bucket
 			reward = 1.0;
 		}
@@ -186,13 +189,13 @@ class Tosser {
 		std::array<double, 10> observation;
 		for (int i = 0; i < 5; i++)
 		{
-			observation[i+5] = env.d->qpos[i];
-			observation[i] = env.d->qvel[i];
+			observation[i+5] = d->qpos[i];
+			observation[i] = d->qvel[i];
 		}
 		
 		//process termination
 		bool done;
-		if (env.d->time > 5 || reward != 0.0)
+		if (d->time > 5 || reward != 0.0)
 		{
 			done = true;
 		}
@@ -211,20 +214,19 @@ class Tosser {
 		return output;
 	}
 
-
 	//reset function
 	std::tuple <std::array<double, 10>, std::string> reset()
 	{
 		//reset env and step forward
-		mj_resetData(env.m, env.d);
-		mj_step(env.m, env.d);
+		mj_resetData(m, d);
+		mj_step(m, d);
 
 		//process observation
 		std::array<double, 10> observation;
 		for (int i = 0; i < 5; i++)
 		{
-			observation[i+5] = env.d->qpos[i];
-			observation[i] = env.d->qvel[i];
+			observation[i+5] = d->qpos[i];
+			observation[i] = d->qvel[i];
 		}
 
 		//create output
@@ -237,6 +239,18 @@ class Tosser {
 	//render function
 	unsigned char* render()
 	{
-		return env.render();
+		if (b_render) {
+			return env->render(m, d);
+		}
+		else {
+			mju_error("LightEnvironment trying to render!");
+		}
+	}
+
+	~Tosser()
+	{
+		mj_deleteData(d);
+		mj_deleteModel(m);
+		delete env;
 	}
 };
